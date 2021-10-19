840802513dcf8386aeae5566d2d98cf9b2667f8a
Royer <rquispeccoya@gmail.com>
2021-10-19 13:08:04 -0500

    Creacion de la tabla visitaMedica

--- app/src/main/java/com/lab04/visitamedicamvvm/ConexionSQLite.java
+++ app/src/main/java/com/lab04/visitamedicamvvm/ConexionSQLite.java
@@ -14,13 +14,11 @@ public class ConexionSQLite extends  SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table pacientes(pacDni INTEGER NOT NULL ,PacNom TEXT NOT NULL ,PacEma TEXT NOT NULL,CONSTRAINT PK_pacientes PRIMARY KEY(pacDni))");
-       /* db.execSQL("create table deporte(DepCod INTEGER NOT NULL ,DepNom TEXT NOT NULL ,DepEst VARCHAR(1) NOT NULL,CONSTRAINT PK_deporte PRIMARY KEY(DepCod))");
-        db.execSQL("create table fichaInscripcion(FicInsCod INTEGER NOT NULL ,SocCod INTEGER ,DepCod INTEGER,FicInsEst VARCHAR(1) NOT NULL," +
-                "CONSTRAINT PK_fichaInscripcion PRIMARY KEY(FicInsCod),CONSTRAINT Relationship1 FOREIGN KEY (SocCod) REFERENCES socios (SocCod)," +
-                "CONSTRAINT Relationship3 FOREIGN KEY (DepCod) REFERENCES deporte (DepCod))");
-        db.execSQL("CREATE INDEX IX_Relationship1 ON fichaInscripcion (SocCod);");
-        db.execSQL("CREATE INDEX IX_Relationship3 ON fichaInscripcion (DepCod);");
-    */
+        db.execSQL("create table visitaMedica(VisMedCod INTEGER  NOT NULL ,PacDni INTEGER,VisMedPes INTEGER,VisMedTem INTEGER, VisMedSat INTEGER, VisMedPre INTEGER," +
+                "CONSTRAINT PK_visitaMedica PRIMARY KEY(VisMedCod) ,CONSTRAINT Relationship1 FOREIGN KEY (PacDni) REFERENCES pacientes (PacDni))");
+       db.execSQL("CREATE INDEX IX_Relationship1 ON visitaMedica (PacDni);");
+      //  db.execSQL("CREATE INDEX IX_Relationship3 ON fichaInscripcion (DepCod);");
+
 
     }
 
@@ -28,7 +26,7 @@ public class ConexionSQLite extends  SQLiteOpenHelper{
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS pacientes");
-       // db.execSQL("DROP TABLE IF EXISTS deporte");
+        db.execSQL("DROP TABLE IF EXISTS visitaMedica");
         //db.execSQL("DROP TABLE IF EXISTS fichaInscripcion");
         onCreate(db);
 
